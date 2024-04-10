a1ed6ee3c987d68b8683252b44eb2acd52c7c0e5
Jonathan Varghese <jonathanvarghese24@gmail.com>
2024-04-10 09:27:34 -0500

    Initial Commit

---
+++ Duck Clicker/Duck_ClickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Duck_ClickerApp.swift
+//  Duck Clicker
+//
+//  Created by Jonathan Varghese on 4/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Duck_ClickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
