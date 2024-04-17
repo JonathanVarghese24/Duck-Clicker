351262143fe8e0fa65487a5bedb8af62d23078ed
Jonathan Varghese <jonathanvarghese24@gmail.com>
2024-04-17 09:39:00 -0500

    updated start menu + 2nd view transportation

--- Duck Clicker/ContentView.swift
+++ Duck Clicker/ContentView.swift
@@ -9,36 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Color.black.opacity(0.6).ignoresSafeArea()
-            VStack {
-                Image("duck")
-                    .frame(width: 10, height: 10)
-                
-                Circle()
-                    .fill(Color.blue)
-                    .frame(width: 350, height: 350)
-                    .overlay(
-                        
-                        Text("Duck Clicker")
+        NavigationView {
+            NavigationLink(destination: DuckView()) {
+                ZStack {
+                    Color.black.opacity(0.6).ignoresSafeArea()
+                    VStack {
+                        Image("duck")
+                            .frame(width: 10, height: 10)
+                        Circle()
+                            .fill(Color.blue)
+                            .frame(width: 350, height: 350)
+                            .overlay(
+                                Text("Duck Clicker")
+                                    .font(.title)
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.yellow))
+                            .padding()
+                        Text("Click Anywhere to Start")
                             .font(.title)
                             .fontWeight(.bold)
-                            .foregroundColor(.yellow))
+                            .foregroundColor(.white)
+                    }
                     .padding()
-                
-                Button("Click to Start") { 
-                    
                 }
-                    .buttonStyle(.borderedProminent)
-                    .tint(.yellow)
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .foregroundColor(.black)
             }
-            .padding()
+            .navigationTitle("Start")
+            .navigationBarHidden(true)
         }
     }
 }
+
 
 #Preview {
     ContentView()
---
+++ Duck Clicker/DuckView.swift
@@ -0,0 +1,18 @@
+//
+//  DuckView.swift
+//  Duck Clicker
+//
+//  Created by Jonathan Varghese on 4/17/24.
+//
+
+import SwiftUI
+
+struct DuckView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DuckView()
+}
