f58e195b0d633f8e5782c00f6f9268e5ec4edd12
William Dunkel <wndunkel@gmail.com>
2024-04-19 09:46:00 -0500

    Added upgrades view

--- Duck Clicker/DuckView.swift
+++ Duck Clicker/DuckView.swift
@@ -10,23 +10,36 @@ import SwiftUI
 struct DuckView: View {
     @State private var score: Int = 0
     var body: some View {
-        ZStack {
-            Color.blue.opacity(0.8).ignoresSafeArea()
-            VStack {
-                Button(action: {
-                    score = score + 1
-                }) {
-                    Image("duck")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 300, height: 300)
-                }
-                Text("Your Score is \(score)")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .foregroundColor(.white)
-            }
-        }
+        NavigationView{
+            ZStack {
+                Color.blue.opacity(0.8).ignoresSafeArea()
+                VStack {
+                    Button(action: {
+                        score = score + 1
+                    }) {
+                        Image("duck")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 300, height: 300)
+                    }
+                    Text("Your Score is \(score)")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                    NavigationLink(destination: UpgradesView()) {
+                                            Text("Upgrades")
+                                        }
+                                        .buttonStyle(.borderedProminent)
+                                    }
+                                }
+                            }
+                        }
+                    }
+struct UpgradesView: View {
+    var body: some View {
+        Text("Upgrades View")
+            .font(.title)
+            .fontWeight(.bold)
     }
 }
 
