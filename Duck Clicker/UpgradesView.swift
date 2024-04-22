0cce304f6fa6e7088c1a8129efb0e2ca63909025
Jonathan Varghese <jonathanvarghese24@gmail.com>
2024-04-22 09:52:04 -0500

    fixed upgrades view button

--- Duck Clicker/UpgradesView.swift
+++ Duck Clicker/UpgradesView.swift
@@ -6,20 +6,41 @@
 //
 
 import SwiftUI
-
 struct UpgradesView: View {
     @State private var twoscore: Bool = false
     @Binding var clicks: Int
+    @Environment(\.presentationMode) var presentationMode
     var body: some View {
-        VStack {
-            Text("Your Score is \(clicks)")
+        
             
-            Button(action: {
-
-            })
-            Text("Lose 1000 score, but each click is now +2 score")
-                .buttonStyle(.borderedProminent)
-                .navigationBarTitle("Upgrades", displayMode: .inline)
+            ZStack {
+                Color.blue.opacity(0.8).ignoresSafeArea()
+                VStack{
+                    
+                    Text("Upgrades")
+                        .font(.system(size: 50))
+                        .fontWeight(.bold)
+                    Spacer()
+                        .navigationBarBackButtonHidden(true)
+                    VStack {
+                        Text("Your Score is \(clicks)")
+                        
+                        Text("Lose 1000 score, but each click is now +2 score")
+                            .buttonStyle(.borderedProminent)
+                            .navigationBarTitle("Upgrades", displayMode: .inline)
+                            .navigationBarHidden(true)
+                        Spacer()
+                        Button(action: {
+                                      // Go back to the previous view
+                                      presentationMode.wrappedValue.dismiss()
+                                  }) {
+                                      Text("Go Back")
+                                          .foregroundColor(.white)
+                                          .padding()
+                                          .background(Color.blue)
+                                          .cornerRadius(10)
+                                  }}
+            }
         }
     }
 }
